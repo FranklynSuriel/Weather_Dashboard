6708cd4a3e8bd2c19c71f5f18aafe4fb0c2ceecb
Franksu <fsuriel@gmail.com>
2023-01-09 17:36:55 -0500

    add more code icon url working

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,38 @@
 var button = document.querySelector('.btn');
 var inputValue = document.querySelector('.inputValue');
 var cityName = document.querySelector('.cityName');
-var icon = document.querySelector('#icon');
+var icon = $('#icon');
 var temperature = document.querySelector('.temp');
 var windSpeed = document.querySelector('.windSpeed');
 var humidity = document.querySelector('.humidity');
 
 
-button.addEventListener('click', function () {
+function searchCity() {
 
     fetch('https://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&appid=232b6c46052204d8ef059e1facae5a19')
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            console.log(data)
+        .then(function (data) {           
             var latitud = data[0].lat;
             var longitud = data[0].lon;
 
-            console.log(latitud);
-            console.log(longitud);
-            
-            
             fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitud + '&lon=' + longitud + '&appid=232b6c46052204d8ef059e1facae5a19&units=imperial')
             .then(function (response) {
                 return response.json();
             })
-            .then(function (data) {
-                console.log(data)
+            .then(function (data) {              
                 var nameValue = data['name'];
                 var tempValue = data['main']['temp'];
                 var iconValue = data['weather'][0]['icon'];
-                var iconUrl = 'http://openweathermap.org/img/wn/'+iconValue+'@2x.png'
-                // icon.setAttribute('href', iconUrl)
-                $('#icon').attr('src', iconUrl);
+                var iconUrl = 'https://openweathermap.org/img/wn/'+iconValue+'.png';                              
+                icon.attr('src', iconUrl);
+                icon.attr('alt','Weather Icon')
                 var windSpeedValue = data['wind']['speed'];
                 var humidityValue = data['main']['humidity'];
                 
-                
-               
-                
                 cityName.innerHTML = nameValue;
+                $('#currentDay').text(dayjs().format('MM/D/YYYY'));               
                 temperature.innerHTML = 'Temp: ' + tempValue + ' °F';
                 // icon.innerHTML =  'http://openweathermap.org/img/wn/'+iconValue+'@2x.png'
                 windSpeed.innerHTML = 'Wind: ' + windSpeedValue + ' mph';
@@ -51,14 +43,37 @@ button.addEventListener('click', function () {
 
 
         .catch(err => alert("Wrong city name!"))
+    forecast();
 
+}
 
+function forecast(){
+    var forecastTitle = document.querySelector('.forecast')
 
-    setInterval(function() {
+    forecastTitle.innerHTML = "5-Day Forecast"
+
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&appid=232b6c46052204d8ef059e1facae5a19')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+        console.log(data)
+        var latitud = data[0].lat;
+        var longitud = data[0].lon;
         
-        $('#currentDay').text(dayjs().format('MM/D/YYYY'));               
-    }, 1000);
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitud + '&lon=' + longitud + '&appid=232b6c46052204d8ef059e1facae5a19&units=imperial')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            
+        })
+    })
+        
+  
 
+}
 
-})
-
+button.addEventListener('click',searchCity);
